59c2f071d0286e7e845ab4db694e50d53b0f24b2
Yuki Miyasaka <yuki.m5911@gmail.com>
2018-06-01 01:14:48 +0000

    CRUD messages

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>タスク一覧</h1>
+
+    @if (count($tasklists) > 0)
+        <ul>
+             @foreach ($tasklists as $tasklist)
+                <li>{!! link_to_route('tasklists.show', $tasklist->id, ['id' => $tasklist->id]) !!} : {{ $tasklist->content }}</li>
+            @endforeach
+        </ul>
+    @endif
+{!! link_to_route('tasklists.create', '新規タスクの投稿') !!}
+
+@endsection
